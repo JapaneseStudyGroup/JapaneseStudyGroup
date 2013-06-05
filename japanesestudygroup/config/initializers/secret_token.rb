3b31e868d9bd3e6397db2c04baf19896b512ff44
Tommy Joe Rogers III <rogerst8@rogershost.(none)>
2013-06-04 18:44:57 -0700

    Adding the ruby framework for the site.

---
+++ japanesestudygroup/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Japanesestudygroup::Application.config.secret_token = '54332da52ac6e35e4924f4353e3f1c07b493b5fff98132e26d4992da0e5be44d63d263bbbcb7931d406265013d02dc8e1877165a4c660f84a73c39f6a9fc3cd7'
